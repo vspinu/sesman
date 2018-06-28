0d8d1bef455b58439df333f2a0a0693543b8f098
Vitalie Spinu <spinuvit@gmail.com>
2018-06-28 13:48:59 +0200

    Version 0.1.1

--- sesman.el
+++ sesman.el
@@ -4,7 +4,7 @@
 ;; Author: Vitalie Spinu
 ;; URL: https://github.com/vspinu/sesman
 ;; Keywords: process
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "25"))
 ;; Keywords: processes, tools, vc
 ;;
